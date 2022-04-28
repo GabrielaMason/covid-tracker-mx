49d70da0ccfc056cfd222f788bd2a1e38872a251
GabrielaMason <0202578@up.edu.mx>
2022-04-27 19:39:08 -0500

    Mejoras y parte incial de la seccion de probabilidades

--- src/Components/GralData/GralDataView.js
+++ src/Components/GralData/GralDataView.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function GralData(){
     return (
-        <div id="tracker">
+        <div id="tracker" className="mb-3">
             <h1>Datos gráficos del Covid</h1>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, repellendus. Totam nihil similique a repellat minus dolor amet quasi. Corporis nulla quaerat iste, sed quasi ab dolorem maxime minima animi.
         </div>
