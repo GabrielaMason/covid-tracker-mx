a1c1ef50ac9b3d57646d5e73dc1cb446a5566b76
Roberto-RRF <H@1@madr1D>
2022-04-27 21:51:08 -0500

    Query con información filtrada por estado

--- AzureServices/db.js
+++ AzureServices/db.js
@@ -17,9 +17,7 @@ sql.connect(config, err => {
     console.log("Connection Successful !");
 
     new sql.Request().query('select 1 as number', (err, result) => {
-        //handle err
-        console.dir(result)
-        // This example uses callbacks strategy for getting results.
+        return
     })
 
 });
@@ -33,21 +31,60 @@ sql.on('error', err => {
 
 const fetchData = async () => {
 
+
         try {
             // make sure that any items are correctly URL encoded in the connection string
             await sql.connect(config)
-            const result = await sql.query("SELECT * FROM estado")
-            return result.recordset
+            const resEstados = await sql.query("" +
+                "SELECT estado.nombre, covid_info.fecha, covid_info.nuevos_casos, covid_info.nuevas_muertes, estado.id FROM estado \n" +
+                "INNER JOIN covid_info\n" +
+                "ON covid_info.estado_id = estado.id ")
+
+
+            //Filtramos los datos para agruparlos por estado
+            const DATA = await resEstados.recordset.reduce(function (r, a) {
+                r[a.nombre] = r[a.nombre] || [];
+                r[a.nombre].push(a);
+                return r;
+            }, Object.create(null));
+
+            // //Buscamos en la base de datos la informacion de mexico pais
+            // const resPais = await sql.query("SELECT covid_info.fecha, SUM(covid_info.nuevos_casos) as casos, SUM(covid_info.nuevas_muertes) as muertes FROM estado \n" +
+            //     "INNER JOIN covid_info\n" +
+            //     "ON covid_info.estado_id = estado.id \n" +
+            //     "GROUP By covid_info.fecha\n")
+            //
+            // //Convertimos la fecha a clase Date
+            // const fechaObjeto = resPais.recordset.map(function (a) {
+            //     a.fecha = new Date(a.fecha);
+            //     return a;
+            // });
+            //
+            // console.log(fechaObjeto);
+
+
+
+
+
+
+
+            return DATA;
+
         } catch (err) {
             console.log(err)
         }
+
+
 
 }
 
 
 
 fetchData().then((r) => {
-    console.log(r[0]);
+    // for(let key in r){
+    //     console.log(key, r[key].length)
+    // }
+
 }).catch((e) => {
     console.log(e)
 })
