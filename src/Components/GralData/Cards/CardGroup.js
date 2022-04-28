d1ccedddf5e3f978ebec518575c97f0da18c6143
GabrielaMason <0202578@up.edu.mx>
2022-04-27 23:38:02 -0500

    CountUP

---
+++ src/Components/GralData/Cards/CardGroup.js
@@ -0,0 +1,26 @@
+import React from "react";
+import CardItem from "./CardItem";
+
+export default function CardGroup(props) {
+  const data = props.cardInfo.data;
+  const date = props.cardInfo.date;
+
+  return (
+    <div>
+      <div className="d-flex justify-content-around">
+        {(data && date) ? data.map((d) => {
+          return (
+            <CartItem
+              key={Math.random()}
+              title={d.title}
+              data={d.amount}
+              date={date}
+              description={d.description}
+              type={d.type}
+            />
+          );
+        }):null}
+      </div>
+    </div>
+  );
+};
--- src/Components/GralData/Cards/CardItem.js
+++ src/Components/GralData/Cards/CardItem.js
@@ -1,11 +1,14 @@
 import React from "react";
+import CountUp from "react-countup";
 
-export default function CardItem(){
+export default function CardItem(props){
     return(
         <div className="card" style={{width: "18rem"}}>
             <div className="card-body">
                 <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                <h5 className="card-title">Card subtitle</h5>
+                <h5 className="card-title">
+                    <CountUp start={0} end={50} duration={2.5} separator=","/>
+                </h5>
                 <p className="card-text">Date.</p>
                 <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
             </div>
